7353a41522a9d32203b2b8bbcafc5ec3c4a080eb
aZYiming <imvainycos@foxmail.com>
2020-04-07 11:02:31 +0800

    this is a commit

--- src/main/java/com/vainycos/dear/dal/Vainycos.java
+++ src/main/java/com/vainycos/dear/dal/Vainycos.java
@@ -5,16 +5,14 @@ public class Vainycos {
     public String getName(String name){
         if(name.contains("v")){
             return name;
-        }else {
-            return "whose your daddy";
         }
+        return "this is demo branch";
     }
 
     public String demoBranch(String name){
         if(name.contains("contains")){
-            return "branch";
-        }else {
-            return "";
+            return "demo branch";
         }
+        return "";
     }
 }
